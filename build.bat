075bc75aa48b44251a8be3f2b69f9c3b9afc1d07
ThreewayDude <dutta.hritik@gmail.com>
2021-04-13 14:49:29 +0530

    Build: Added args folder to build system

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 
 cl /c filestuff\*.c /I .
 cl /c todos\*.c /I .
+cl /c args\*.c /I .
 cl main.c /Fe:todo-test *.obj
 
 del *.obj
--- install.bat
+++ install.bat
@@ -6,6 +6,7 @@ if not exist %outdir%todo-data.txt type nul > %outdir%todo-data.txt
 
 cl /c filestuff\*.c /I .
 cl /c todos\*.c /I .
+cl /c args\*.c /I .
 cl main.c /Fe:%outdir%todo *.obj
 
 del *.obj
